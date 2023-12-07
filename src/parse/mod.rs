e57c69b6c99fa31c43507912b7c4d0eec79e8d72
Trevor Apple <trevorapple@Trevors-MacBook-Air.local>
2023-12-06 16:05:53 -0800

    more work on the parser

--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -10,7 +10,7 @@ binary -> expression operator expression;
 operator -> "+" | "-" | "*" | "/" | "==" | "!=" | "<" | "<=" | ">"" | ">=";
 grouping -> "(" expression ")"
 
-expression -> brace_expression | if | if_else | for_loop | equality;
+expression -> brace_expression | if_else | for_loop | equality;
 brace_expression -> "{" brace_statements? expression "}";
 brace_statements -> brace_statements? (brace_expression | statement | if_else);
 statement -> SYMBOL "=" expression ";" | expression ";";
@@ -19,7 +19,7 @@ for_loop -> "for" "(" expression ";" expression ";" expression ";" ")" brace_exp
 equality -> (equality ("==" | "!=") comparison) | comparison;
 comparison -> (comparison (">" | ">=" | "<" | "<=") plus_minus) | plus_minus;
 plus_minus -> (plus_minus ("+" | "-") mult_div) | mult_div;
-mult_div -> (expression ("*" | "/") expression) | boolean;
+mult_div -> (mult_div ("*" | "/") unary) | unary;
 unary -> (("!" | "-") unary) | primary;
 primary -> TRUE | FALSE | SYMBOL | NUMBER | STRING | "(" expression ")";
 */
@@ -34,11 +34,21 @@ but what about flow control? it's just another type of node i suppose
 break? early return?
 named loops?
 function calls?
+data structures
 */
+
+mod token_search;
 
 use std::todo;
 
 use crate::tokenize::tokens::{Token, Tokens};
+
+use self::token_search::{
+    find_final_matching_level_token, find_final_token,
+    find_matching_group_indices, find_matching_group_indices_end,
+    find_next_matching_level_token, find_next_token,
+    find_prev_matching_level_token,
+};
 
 pub struct Ast {}
 
@@ -87,12 +97,24 @@ pub fn parse(tokens: &Tokens) -> Ast {
             Rule::IfElse => {
                 parse_if_else_rule(tokens, &search_data, &mut stack);
             }
-            Rule::ForLoop => todo!(),
-            Rule::Equality => todo!(),
-            Rule::Comparison => todo!(),
-            Rule::PlusMinus => todo!(),
-            Rule::MultDiv => todo!(),
-            Rule::Unary => todo!(),
+            Rule::ForLoop => {
+                parse_for_rule(tokens, &search_data, &mut stack);
+            }
+            Rule::Equality => {
+                parse_equality_rule(tokens, &search_data, &mut stack);
+            }
+            Rule::Comparison => {
+                parse_comparison_rule(tokens, &search_data, &mut stack);
+            }
+            Rule::PlusMinus => {
+                parse_plus_minus_rule(tokens, &search_data, &mut stack);
+            }
+            Rule::MultDiv => {
+                parse_mult_div_rule(tokens, &search_data, &mut stack);
+            }
+            Rule::Unary => {
+                parse_unary_rule(tokens, &search_data, &mut stack);
+            }
             Rule::Primary => todo!(),
         }
     }
@@ -113,108 +135,36 @@ fn parse_expression_rule(
         Some(token) => token,
         None => todo!("Parse error (panic?)"),
     };
-    if *start_token == Token::LBrace {
-        if *end_token != Token::RBrace {
-            todo!("Syntax error");
-        }
-
-        // search for last endstatement before the rbrace
-        match find_final_token(
-            tokens,
-            &Token::EndStatement,
-            search_data.start,
-            search_data.end,
-        ) {
-            Some(expression_start_index) => {
-                // expression at end of braces
-                stack.push(SearchData {
-                    start: expression_start_index,
-                    end: search_data.end - 1,
-                    rule: Rule::Expression,
-                });
-                // statements preceding expression
-                stack.push(SearchData {
-                    start: search_data.start + 1,
-                    end: expression_start_index,
-                    rule: Rule::BraceStatements,
-                })
-            }
-            None => {
-                // no leading statements
-            }
-        }
+    let rule = if *start_token == Token::LBrace {
+        Rule::BraceExpression
     } else if *start_token == Token::If {
-        if *end_token != Token::RBrace {
-            todo!("Syntax error");
-        }
-
-        let else_index: Option<usize> = {
-            let mut else_index = None;
-            for search_index in search_data.start..search_data.end {
-                match tokens.get(search_index) {
-                    Some(token) => {
-                        if *token == Token::Else {
-                            else_index = Some(search_index);
-                            break;
-                        }
-                    }
-                    None => todo!("Panic?"),
-                }
-            }
-
-            else_index
-        };
-
-        match else_index {
-            Some(else_index) => {
-                // verify that else index is preceded by closing bracket
-                match tokens.get(else_index - 1) {
-                    Some(check_token) => {
-                        if *check_token != Token::RBrace {
-                            todo!("Syntax error");
-                        }
-                    }
-                    None => todo!("Panic?"),
-                };
-
-                // if
-                stack.push(SearchData {
-                    start: search_data.start + 1,
-                    end: else_index - 1,
-                    rule: Rule::BraceExpression,
-                });
-
-                // else
-                stack.push(SearchData {
-                    start: else_index + 1,
-                    end: search_data.end,
-                    rule: Rule::BraceExpression,
-                });
-            }
-            None => stack.push(SearchData {
-                start: search_data.start + 1,
-                end: search_data.end - 1,
-                rule: Rule::BraceExpression,
-            }),
-        }
+        Rule::IfElse
     } else if *start_token == Token::For {
-        expand_for_expression_rule(stack, tokens, search_data, end_token);
+        Rule::ForLoop
     } else {
-        // try to parse as expression
-    }
-    todo!()
+        Rule::Equality
+    };
+
+    stack.push(SearchData {
+        start: search_data.start,
+        end: search_data.end,
+        rule,
+    });
 }
 
-/// helper function for expanding an expression rule that seems to match a for
-/// loop
-fn expand_for_expression_rule(
-    stack: &mut Vec<SearchData>,
+/// parses the for rule
+fn parse_for_rule(
     tokens: &Tokens,
     search_data: &SearchData,
-    end_token: &Token,
+    stack: &mut Vec<SearchData>,
 ) {
-    if *end_token != Token::RBrace {
-        todo!("Syntax error");
+    match tokens.get(search_data.end) {
+        Some(end_token) => {
+            if *end_token != Token::RBrace {
+                todo!("Syntax error")
+            }
+        }
+        None => todo!("Syntax error"),
     }
 
     let lparen_index = search_data.start + 1;
@@ -533,14 +483,12 @@ fn parse_statement_rule(
     }
 }
 
-///
+/// parse the if_else rule
 fn parse_if_else_rule(
     tokens: &Tokens,
     search_data: &SearchData,
     stack: &mut Vec<SearchData>,
 ) {
-    // if_else -> "if" expression brace_expression ("else" expression)?;
-
     match tokens.get(search_data.start) {
         Some(expected_if) => {
             if *expected_if != Token::If {
@@ -625,208 +573,169 @@ fn parse_if_else_rule(
     }
 }
 
-/// finds the indices of the matching rtoken for the first ltoken found at
-/// starts_at
-fn find_matching_group_indices(
+// parses the equality rule
+fn parse_equality_rule(
     tokens: &Tokens,
-    ltoken: &Token,
-    rtoken: &Token,
-    starts_at: usize,
-    ends_at: usize,
-) -> Option<usize> {
-    let mut ltokens_found = 1;
-    let mut rtokens_found = 0;
-
-    for index in (starts_at + 1)..ends_at {
-        if let Some(token) = tokens.get(index) {
-            if *token == *ltoken {
-                ltokens_found += 1;
-            } else if *token == *rtoken {
-                rtokens_found += 1;
-            }
-
-            if ltokens_found == rtokens_found {
-                return Some(index);
-            }
-        } else {
-            break;
+    search_data: &SearchData,
+    stack: &mut Vec<SearchData>,
+) {
+    match find_final_matching_level_token(
+        tokens,
+        &[Token::BoolEquals, Token::NotEquals],
+        search_data.start,
+        search_data.end,
+        &Token::LParen,
+        &Token::RParen,
+    ) {
+        Some(split_index) => {
+            stack.push(SearchData {
+                start: search_data.start,
+                end: split_index,
+                rule: Rule::Equality,
+            });
+            stack.push(SearchData {
+                start: split_index + 1,
+                end: search_data.end,
+                rule: Rule::Comparison,
+            });
         }
+        None => stack.push(SearchData {
+            start: search_data.start,
+            end: search_data.end,
+            rule: Rule::Comparison,
+        }),
     }
-
-    None
 }
 
-/// finds the indices of the matching ltoken for the rtoken found at
-/// ends_at
-fn find_matching_group_indices_end(
+/// parse comparison rule
+fn parse_comparison_rule(
     tokens: &Tokens,
-    ltoken: &Token,
-    rtoken: &Token,
-    starts_at: usize,
-    ends_at: usize,
-) -> Option<usize> {
-    let mut ltokens_found = 0;
-    let mut rtokens_found = 1;
-
-    for index in (starts_at..(ends_at - 1)).rev() {
-        if let Some(token) = tokens.get(index) {
-            if *token == *ltoken {
-                ltokens_found += 1;
-            } else if *token == *rtoken {
-                rtokens_found += 1;
-            }
-
-            if ltokens_found == rtokens_found {
-                return Some(index);
-            }
-        } else {
-            break;
+    search_data: &SearchData,
+    stack: &mut Vec<SearchData>,
+) {
+    match find_final_matching_level_token(
+        tokens,
+        &[
+            Token::GreaterThan,
+            Token::GreaterThanOrEqual,
+            Token::LessThan,
+            Token::LessThanOrEqual,
+        ],
+        search_data.start,
+        search_data.end,
+        &Token::LParen,
+        &Token::RParen,
+    ) {
+        Some(split_index) => {
+            stack.push(SearchData {
+                start: search_data.start,
+                end: split_index,
+                rule: Rule::Comparison,
+            });
+            stack.push(SearchData {
+                start: split_index + 1,
+                end: search_data.end,
+                rule: Rule::PlusMinus,
+            });
         }
+        None => stack.push(SearchData {
+            start: search_data.start,
+            end: search_data.end,
+            rule: Rule::PlusMinus,
+        }),
     }
-
-    None
 }
 
-/// finds the index of the final token between starts_at and ends_at
-/// (starts_at <= index < ends_at). Returns None if not found
-/// searches in reverse
-fn find_final_token(
+/// parse plus_minus rule
+fn parse_plus_minus_rule(
     tokens: &Tokens,
-    token: &Token,
-    starts_at: usize,
-    ends_at: usize,
-) -> Option<usize> {
-    for index in (starts_at..ends_at).rev() {
-        if let Some(check_token) = tokens.get(index) {
-            if *check_token == *token {
-                return Some(index);
-            }
-        } else {
-            return None;
+    search_data: &SearchData,
+    stack: &mut Vec<SearchData>,
+) {
+    match find_final_matching_level_token(
+        tokens,
+        &[Token::Plus, Token::Minus],
+        search_data.start,
+        search_data.end,
+        &Token::LParen,
+        &Token::RParen,
+    ) {
+        Some(split_index) => {
+            stack.push(SearchData {
+                start: search_data.start,
+                end: split_index,
+                rule: Rule::PlusMinus,
+            });
+            stack.push(SearchData {
+                start: split_index + 1,
+                end: search_data.end,
+                rule: Rule::MultDiv,
+            });
         }
+        None => stack.push(SearchData {
+            start: search_data.start,
+            end: search_data.end,
+            rule: Rule::MultDiv,
+        }),
     }
-
-    None
 }
 
-/// finds the index of the final token between starts_at and ends_at
-/// (starts_at <= index < ends_at) that is at the same grouping level as
-/// starts_at
-///
-/// Returns None if not found
-fn find_final_matching_level_token(
+/// parse mult div
+fn parse_mult_div_rule(
     tokens: &Tokens,
-    matching_tokens: &[Token],
-    starts_at: usize,
-    ends_at: usize,
-    group_start_token: &Token,
-    group_end_token: &Token,
-) -> Option<usize> {
-    let mut result: Option<usize> = None;
-
-    let mut current_level = 0;
-    for index in starts_at..ends_at {
-        if let Some(check_token) = tokens.get(index) {
-            if *check_token == *group_start_token {
-                current_level += 1;
-            } else if *check_token == *group_end_token {
-                current_level -= 1;
-            } else if current_level == 0
-                && matching_tokens.contains(check_token)
-            {
-                result = Some(index);
-            }
-
-            if current_level < 0 {
-                // we have moved outside of the grouping that starts_at was in
-                return None;
-            }
-        } else {
-            return None;
+    search_data: &SearchData,
+    stack: &mut Vec<SearchData>,
+) {
+    match find_final_matching_level_token(
+        tokens,
+        &[Token::Times, Token::Divide],
+        search_data.start,
+        search_data.end,
+        &Token::LParen,
+        &Token::RParen,
+    ) {
+        Some(split_index) => {
+            stack.push(SearchData {
+                start: search_data.start,
+                end: split_index,
+                rule: Rule::MultDiv,
+            });
+            stack.push(SearchData {
+                start: split_index + 1,
+                end: search_data.end,
+                rule: Rule::Unary,
+            });
         }
+        None => stack.push(SearchData {
+            start: search_data.start,
+            end: search_data.end,
+            rule: Rule::Unary,
+        }),
     }
-
-    result
 }
 
-/// finds the index of the next token after starts_at and before ends_at
-/// (starts_at <= index < ends_at) that is at the same grouping level as
-/// starts_at
-///
-/// returns None if not found
-fn find_next_matching_level_token(
+fn parse_unary_rule(
     tokens: &Tokens,
-    matching_tokens: &[Token],
-    starts_at: usize,
-    ends_at: usize,
-    group_start_token: &Token,
-    group_end_token: &Token,
-) -> Option<usize> {
-    let mut current_level = 0;
-
-    for index in starts_at..ends_at {
-        if let Some(check_token) = tokens.get(index) {
-            if current_level == 0 && matching_tokens.contains(check_token) {
-                return Some(index);
-            } else if *check_token == *group_start_token {
-                current_level += 1;
-            } else if *check_token == *group_end_token {
-                current_level -= 1;
-            }
-        } else {
-            return None;
-        }
-    }
-
-    None
-}
-
-/// finds the index of the token previous token before ends_at and after
-/// starts_at (starts_at <= index < ends_at) that is at the same grouping level
-/// as ends_at
-///
-/// returns None if not found
-fn find_prev_matching_level_token(
-    tokens: &Tokens,
-    matching_tokens: &[Token],
-    starts_at: usize,
-    ends_at: usize,
-    group_start_token: &Token,
-    group_end_token: &Token,
-) -> Option<usize> {
-    let mut current_level = 0;
-
-    for index in (starts_at..ends_at).rev() {
-        if let Some(check_token) = tokens.get(index) {
-            if current_level == 0 && matching_tokens.contains(check_token) {
-                return Some(index);
-            } else if *check_token == *group_start_token {
-                current_level -= 1;
-            } else if *check_token == *group_end_token {
-                current_level += 1;
-            }
-        } else {
-            return None;
-        }
-    }
-
-    None
-}
-
-/// finds the index of the next token of a certain type
-fn find_next_token(
-    tokens: &Tokens,
-    token: &Token,
-    starts_at: usize,
-    ends_at: usize,
-) -> Option<usize> {
-    for index in starts_at..ends_at {
-        if let Some(check_token) = tokens.get(index) {
-            if *check_token == *token {
-                return Some(index);
+    search_data: &SearchData,
+    stack: &mut Vec<SearchData>,
+) {
+    // unary -> (("!" | "-") unary) | primary;
+    match tokens.get(search_data.start) {
+        Some(first_token) => {
+            if *first_token == Token::Not || *first_token == Token::Minus {
+                stack.push(SearchData {
+                    start: search_data.start + 1,
+                    end: search_data.end,
+                    rule: Rule::Unary,
+                });
+            } else {
+                stack.push(SearchData {
+                    start: search_data.start,
+                    end: search_data.end,
+                    rule: Rule::Primary,
+                });
             }
         }
+        None => todo!("Syntax error"),
     }
-
-    None
 }
---
+++ src/parse/token_search.rs
@@ -0,0 +1,225 @@
+use crate::tokenize::tokens::{Token, Tokens};
+
+/// finds the indices of the matching rtoken for the first ltoken found at
+/// starts_at
+pub fn find_matching_group_indices(
+    tokens: &Tokens,
+    ltoken: &Token,
+    rtoken: &Token,
+    starts_at: usize,
+    ends_at: usize,
+) -> Option<usize> {
+    let mut ltokens_found = 1;
+    let mut rtokens_found = 0;
+
+    for index in (starts_at + 1)..ends_at {
+        if let Some(token) = tokens.get(index) {
+            if *token == *ltoken {
+                ltokens_found += 1;
+            } else if *token == *rtoken {
+                rtokens_found += 1;
+            }
+
+            if ltokens_found == rtokens_found {
+                return Some(index);
+            }
+        } else {
+            break;
+        }
+    }
+
+    None
+}
+
+/// finds the indices of the matching ltoken for the rtoken found at
+/// ends_at
+pub fn find_matching_group_indices_end(
+    tokens: &Tokens,
+    ltoken: &Token,
+    rtoken: &Token,
+    starts_at: usize,
+    ends_at: usize,
+) -> Option<usize> {
+    let mut ltokens_found = 0;
+    let mut rtokens_found = 1;
+
+    for index in (starts_at..(ends_at - 1)).rev() {
+        if let Some(token) = tokens.get(index) {
+            if *token == *ltoken {
+                ltokens_found += 1;
+            } else if *token == *rtoken {
+                rtokens_found += 1;
+            }
+
+            if ltokens_found == rtokens_found {
+                return Some(index);
+            }
+        } else {
+            break;
+        }
+    }
+
+    None
+}
+
+/// finds the index of the final token between starts_at and ends_at
+/// (starts_at <= index < ends_at). Returns None if not found
+/// searches in reverse
+pub fn find_final_token(
+    tokens: &Tokens,
+    token: &Token,
+    starts_at: usize,
+    ends_at: usize,
+) -> Option<usize> {
+    for index in (starts_at..ends_at).rev() {
+        if let Some(check_token) = tokens.get(index) {
+            if *check_token == *token {
+                return Some(index);
+            }
+        } else {
+            return None;
+        }
+    }
+
+    None
+}
+
+/// finds the index of the final token between starts_at and ends_at
+/// (starts_at <= index < ends_at) that is at the same grouping level as
+/// starts_at
+///
+/// Returns None if not found
+pub fn find_final_matching_level_token(
+    tokens: &Tokens,
+    matching_tokens: &[Token],
+    starts_at: usize,
+    ends_at: usize,
+    group_start_token: &Token,
+    group_end_token: &Token,
+) -> Option<usize> {
+    let mut result: Option<usize> = None;
+
+    let mut current_level = 0;
+    for index in starts_at..ends_at {
+        if let Some(check_token) = tokens.get(index) {
+            if *check_token == *group_start_token {
+                current_level += 1;
+            } else if *check_token == *group_end_token {
+                current_level -= 1;
+            } else if current_level == 0
+                && matching_tokens.contains(check_token)
+            {
+                result = Some(index);
+            }
+
+            if current_level < 0 {
+                // we have moved outside of the grouping that starts_at was in
+                return None;
+            }
+        } else {
+            return None;
+        }
+    }
+
+    result
+}
+
+/// finds the index of the next token after starts_at and before ends_at
+/// (starts_at <= index < ends_at) that is at the same grouping level as
+/// starts_at
+///
+/// returns None if not found
+pub fn find_next_matching_level_token(
+    tokens: &Tokens,
+    matching_tokens: &[Token],
+    starts_at: usize,
+    ends_at: usize,
+    group_start_token: &Token,
+    group_end_token: &Token,
+) -> Option<usize> {
+    let mut current_level = 0;
+
+    for index in starts_at..ends_at {
+        if let Some(check_token) = tokens.get(index) {
+            if current_level == 0 && matching_tokens.contains(check_token) {
+                return Some(index);
+            } else if *check_token == *group_start_token {
+                current_level += 1;
+            } else if *check_token == *group_end_token {
+                current_level -= 1;
+            }
+        } else {
+            return None;
+        }
+    }
+
+    None
+}
+
+/// finds the index of the token previous token before ends_at and after
+/// starts_at (starts_at <= index < ends_at) that is at the same grouping level
+/// as ends_at
+///
+/// returns None if not found
+pub fn find_prev_matching_level_token(
+    tokens: &Tokens,
+    matching_tokens: &[Token],
+    starts_at: usize,
+    ends_at: usize,
+    group_start_token: &Token,
+    group_end_token: &Token,
+) -> Option<usize> {
+    let mut current_level = 0;
+
+    for index in (starts_at..ends_at).rev() {
+        if let Some(check_token) = tokens.get(index) {
+            if current_level == 0 && matching_tokens.contains(check_token) {
+                return Some(index);
+            } else if *check_token == *group_start_token {
+                current_level -= 1;
+            } else if *check_token == *group_end_token {
+                current_level += 1;
+            }
+        } else {
+            return None;
+        }
+    }
+
+    None
+}
+
+/// finds the index of the next token of a certain type
+pub fn find_next_token(
+    tokens: &Tokens,
+    token: &Token,
+    starts_at: usize,
+    ends_at: usize,
+) -> Option<usize> {
+    for index in starts_at..ends_at {
+        if let Some(check_token) = tokens.get(index) {
+            if *check_token == *token {
+                return Some(index);
+            }
+        }
+    }
+
+    None
+}
+
+/// finds the index of the next token among a few types
+pub fn find_next_tokens(
+    tokens: &Tokens,
+    matching_tokens: &[Token],
+    starts_at: usize,
+    ends_at: usize,
+) -> Option<usize> {
+    for index in starts_at..ends_at {
+        if let Some(check_token) = tokens.get(index) {
+            if matching_tokens.contains(check_token) {
+                return Some(index);
+            }
+        }
+    }
+
+    None
+}
