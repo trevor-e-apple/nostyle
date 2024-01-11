e6afe658cf4b15e033e240f25cf1378d573c470a
Trevor Apple <trevorapple@Trevors-MacBook-Air.local>
2024-01-11 08:42:01 -0800

    used terminal expression common function in another test

--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -1513,21 +1513,7 @@ mod tests {
                 let statement_handle =
                     expected_ast.add_child(statements_handle, Rule::Statement);
 
-                let expression_handle =
-                    expected_ast.add_child(statement_handle, Rule::Expression);
-                let equality_handle =
-                    expected_ast.add_child(expression_handle, Rule::Equality);
-                let comparison_handle =
-                    expected_ast.add_child(equality_handle, Rule::Comparison);
-                let plus_minus_handle =
-                    expected_ast.add_child(comparison_handle, Rule::PlusMinus);
-                let mult_div_handle =
-                    expected_ast.add_child(plus_minus_handle, Rule::MultDiv);
-                let unary_handle =
-                    expected_ast.add_child(mult_div_handle, Rule::Unary);
-                let primary_child =
-                    expected_ast.add_child(unary_handle, Rule::Primary);
-                expected_ast.add_terminal_child(primary_child, None);
+                add_terminal_expression(&mut expected_ast, statement_handle, None);
             }
 
             // no expression at end of brace expression
