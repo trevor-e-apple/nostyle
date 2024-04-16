8d7469e61157c4a3407d8457732de4a7fb07d44c
Trevor Apple <trevorapple@Trevors-MacBook-Air.local>
2024-04-16 15:18:45 -0700

    fixed if_only test

--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -2228,16 +2228,8 @@ mod tests {
                 );
                 // (a + b)
                 {
-                    let recursive_handle =
-                        expected_ast.add_child(equality_handle, Rule::Equality);
-                    let comparison_handle = expected_ast
-                        .add_child(recursive_handle, Rule::Comparison);
-                    let plus_minus_handle = expected_ast
-                        .add_child(comparison_handle, Rule::PlusMinus);
-                    let mult_div_handle = expected_ast
-                        .add_child(plus_minus_handle, Rule::MultDiv);
                     let unary_handle =
-                        expected_ast.add_child(mult_div_handle, Rule::Unary);
+                        expected_ast.add_child(equality_handle, Rule::Unary);
                     let primary_handle =
                         expected_ast.add_child(unary_handle, Rule::Primary);
                     let expression_handle = expected_ast
@@ -2251,14 +2243,8 @@ mod tests {
                 }
                 // c
                 {
-                    let comparison_handle = expected_ast
-                        .add_child(equality_handle, Rule::Comparison);
-                    let plus_minus_handle = expected_ast
-                        .add_child(comparison_handle, Rule::PlusMinus);
-                    let mult_div_handle = expected_ast
-                        .add_child(plus_minus_handle, Rule::MultDiv);
                     let unary_handle =
-                        expected_ast.add_child(mult_div_handle, Rule::Unary);
+                        expected_ast.add_child(equality_handle, Rule::Unary);
                     let primary_handle =
                         expected_ast.add_child(unary_handle, Rule::Primary);
                     expected_ast.add_terminal_child(
